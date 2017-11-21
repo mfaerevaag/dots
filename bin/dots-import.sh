068fafff5fff42da98be0d796905afc55a283d4e
Markus Faerevaag <m@frv.ag>
2017-11-21 15:27:54 +0900

    refactor

--- bin/dots-import.sh
+++ bin/dots-import.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/sh
 
+REPO="git@github.com:mfaerevaag/dots.git"
+DIR="$HOME/.dots"
+BACKUP="$HOME/.dots-backup"
+
 function dots
 {
-   /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$DIR --work-tree=$HOME $@
 }
 
-git clone --bare https://github.com/mfaerevaag/dots.git $HOME/.dots
+git clone --bare $REPO $DIR
 
-mkdir -p .dots-backup
-
+# check if existing
 if dots checkout; then
     echo "checked out dots"
 else
     echo "backing up pre-existing dot files"
-    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    mkdir -p $BACKUP
+    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUP/{}
 fi;
 
+# ignore untracked files
 dots config status.showUntrackedFiles no
