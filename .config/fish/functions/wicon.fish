02484b53c041eb03a25751fdb1b5ebde2f63a8cc
Markus Faerevaag <m@frv.ag>
2018-04-20 12:58:17 +0200

    remove unecessary config

--- .config/fish/functions/wicon.fish
+++
@@ -1,9 +0,0 @@
-# .dotfile
-# fish - wifi connect function
-
-function wicon
-    wires
-    sudo netctl start $argv
-end
-
-complete -c wicon -xa "(netctl list | sed 's/[ *]//g')"
--- .config/fish/functions/wires.fish
+++
@@ -1,7 +0,0 @@
-# .dotfile
-# fish - wifi disconnect function
-
-function wires
-    sudo ip link set wlan0 down
-    sudo netctl stop-all
-end
