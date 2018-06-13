ae907a67c19a87e6d1df2eb2db846375c7050e88
Markus Faerevaag <m@frv.ag>
2018-06-13 19:55:13 +0200

    migrate fish config to oh-my-fish

--- .config/fish/functions/pkgver.fish
+++
@@ -1,3 +0,0 @@
-function pkgver --description 'Print full version of last git tag'
-    echo (git describe --long | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
-end
---
+++ .config/omf/init.fish
@@ -0,0 +1,8 @@
+# aliases
+source $OMF_CONFIG/aliases.fish
+
+# bindings
+source $OMF_CONFIG/bindings.fish
+
+# path
+set -U fish_user_paths ~/bin ~/.local/bin
