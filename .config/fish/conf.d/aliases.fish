02d2d9831d01efbe7b1d818bd1498dfcefa78626
Markus Faerevaag <markus@hugin.io>
2026-01-13 09:09:24 +0100

    Alacritty, fish, sway and waybar

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,31 @@
+function rm
+  command rm -i $argv
+end
+
+function sd
+  sudo systemctl $argv
+end
+
+function sdu
+  systemctl --user $argv
+end
+
+function p
+  sudo pacman $argv
+end
+
+function y
+  yay $argv
+end
+
+function cb
+    command xclip -selection c
+end
+
+function dots
+    command /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $argv
+end
+
+function tf
+    command terraform $argv
+end
